d8134587a56fb14addb5430c9537121916491e95
OmAsati <58726381+OmAsati@users.noreply.github.com>
2020-09-18 19:30:20 +0530

    added fi pages and logo with css

---
+++ build/precache-manifest.a9548dca55968c5c6ad9e9fd6eeda864.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b8d4fb6b61bbab2f5acc5ad0d433f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4994ba4a8f5c067dd054",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "07a32448f6b4242381fd",
+    "url": "/static/js/2.299e5ae0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.299e5ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4994ba4a8f5c067dd054",
+    "url": "/static/js/main.f6ad0094.chunk.js"
+  },
+  {
+    "revision": "f18a821ba95a5c92e9ee",
+    "url": "/static/js/runtime-main.4ff70127.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Donor from "./Components/unAuthComponent/Register/registerDonor";
+import Home from "./Components/authComponent/home";
+function App() {
+  return (
+    <div >
+    <Donor/>
+    </div>
+  );
+}
+
+export default App;
